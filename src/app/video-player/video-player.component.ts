060c8dd0c0af48b55d0cc4dc1c8c1e7a3dea6771
Jacob Mayer <jmayereup@gmail.com>
2023-12-31 09:42:40 +0700

    Update lesson-list.component.css and lesson-list.component.html

---
+++ src/app/video-player/video-player.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-video-player',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './video-player.component.html',
+  styleUrl: './video-player.component.css'
+})
+export class VideoPlayerComponent {
+
+}
