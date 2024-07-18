bdc6e863909740892cc515465f3d5fc7fd6ed4a6
Jacob Mayer <jmayereup@gmail.com>
2024-07-19 04:20:58 +0700

    add discoverroutes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,6 @@ import { routes} from './app.routes'
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideMarkdown } from 'ngx-markdown';
-import { provideServerRendering } from '@angular/platform-server';
 
 export const appConfig: ApplicationConfig = {
   providers: [
