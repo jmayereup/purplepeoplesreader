988e8b632c4eea0dece1e5732f13f062e0d89429
Jacob Mayer <jmayereup@gmail.com>
2024-07-31 19:31:14 +0700

    tweaked images added background

--- src/app/lesson-list/lesson-list.component.ts
+++ src/app/lesson-list/lesson-list.component.ts
@@ -5,12 +5,11 @@ import { stripMarkdown } from '../shared/utils';
 import { RouterLink } from '@angular/router';
 import { SpinnerComponent } from "../spinner/spinner.component";
 import { NavPillsComponent } from "../nav-pills/nav-pills.component";
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-lesson-list',
   standalone: true,
-  imports: [RouterLink, SpinnerComponent, NavPillsComponent, NgOptimizedImage],
+  imports: [RouterLink, SpinnerComponent, NavPillsComponent],
   templateUrl: './lesson-list.component.html',
   styleUrl: './lesson-list.component.css'
 })
